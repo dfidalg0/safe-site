baade4a467070c1b5466d02c366cf003a37eb062
Diego <diego-fidalgo@hotmail.com>
2020-08-28 19:26:52 -0300

    Fixing typo

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,16 @@ if ! [[ -d keys ]]; then
 fi
 
 if ! [[ -d keys/jwt ]]; then
-    mkdir keys/jswt
+    mkdir keys/jwt
 fi
 
 if ! [[ -f keys/jwt/private.key && -f keys/jwt/public.key ]]; then
+    echo "Generating ECDSA keys for JWT..."
     openssl ecparam -genkey -name prime256v1 -noout -out keys/jwt/private.key
     openssl ec -in keys/jwt/private.key -pubout -out keys/jwt/public.key
+    echo "Complete"
+else
+    echo "Keys already exist"
 fi
 
 if ! [[ -d databases ]]; then
