fdfb5ea3418bb1b65098e370ffc83198827653ef
Diego <diego-fidalgo@hotmail.com>
2020-08-28 19:22:57 -0300

    Aditional directory structure

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if ! [[ -d keys ]]; then
+    mkdir keys
+fi
+
+if ! [[ -d keys/jwt ]]; then
+    mkdir keys/jswt
+fi
+
 if ! [[ -f keys/jwt/private.key && -f keys/jwt/public.key ]]; then
     openssl ecparam -genkey -name prime256v1 -noout -out keys/jwt/private.key
     openssl ec -in keys/jwt/private.key -pubout -out keys/jwt/public.key
