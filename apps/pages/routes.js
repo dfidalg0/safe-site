d3cd37680b1427a49741b54f43100486ffc4246c
Diego <diego-fidalgo@hotmail.com>
2020-08-31 19:06:27 -0300

    More front-end stuff

--- apps/pages/routes.js
+++ apps/pages/routes.js
@@ -3,7 +3,15 @@ const { Router } = require('express');
 const routes = Router();
 
 routes.get('/', (req, res) => {
+    return res.render('home.ejs');
+});
+
+routes.get('/login', (req, res) => {
     res.render('login.ejs');
+});
+
+routes.get('/register', (req, res) => {
+    res.render('register.ejs');
 });
 
 module.exports = routes;
